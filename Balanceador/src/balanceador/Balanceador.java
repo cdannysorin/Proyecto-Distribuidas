9479b85c6ffd79199cc058bdd1eac802766e3d32
cdannysorin <danny1991bsc.pm@hotmail.com>
2016-08-15 23:51:36 -0500

    Monitoreo de desastres

---
+++ Balanceador/src/balanceador/Balanceador.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package balanceador;
+
+/**
+ *
+ * @author daniel
+ */
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+
+public class Balanceador {
+    public static void main(String[] args) throws IOException {
+
+    
+    int portNumber = 8000;
+    int i,j=1;
+      
+    try        
+        {
+            ServerSocket serverSocket = new ServerSocket(8000);
+            ArrayList<Socket> clientS=new ArrayList<Socket> ();
+            ArrayList<Thread> threads=new ArrayList<Thread> ();
+            
+            for(i=0;i<j;i++){
+                Socket clientSocket = serverSocket.accept();
+                clientS.add(clientSocket);
+            
+                Thread t1 = new Thread(new Mithread(clientSocket));
+                threads.add(t1);
+                t1.start();
+                j++;
+                
+                //Enrutamiento
+                System.out.println(t1.toString());
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ BashCliente/echoC.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true
+do
+	#free
+	#sleep .1
+	#cd /home/Descargas/Distribuidas/clienteServidorHilos
+	java -jar EchoClient.jar
+done
